0a4b9e1479ac413df7f1191425df444978205ff2
synapticmind <synapticmindai@gmail.com>
2025-07-24 21:12:15 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Truck Fleet Maintenance Dashboard')
+
+st.write("Welcome to the truck fleet maintenance dashboard!")
+
+# Add more components as you build out the app
